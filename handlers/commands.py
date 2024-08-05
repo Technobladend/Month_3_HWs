089f45a3b13f15a3d382942858024d6d208a166a
Kurmanbek-1 <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>
2024-08-05 22:26:11 +0600

    lesson 2

--- handlers/commands.py
+++ handlers/commands.py
@@ -3,14 +3,18 @@ from config import bot
 import os
 from aiogram.types import InputFile
 import random
+from db import db_main
 
 
 # @dp.message_handler(commands=['start'])
 async def start_handler(message: types.Message):
     await bot.send_message(chat_id=message.from_user.id,
                            text='Привет!')
+    # await message.answer(text='Привет')
 
-    await message.answer(text='Привет')
+    print(message.from_user.id)
+    await db_main.sql_insert_registration(telegram_id=message.from_user.id,
+                                          firstname=message.from_user.first_name)
 
 
 async def info_handler(message: types.Message):
